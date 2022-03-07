edcc4563a11cc5c6807d4ffbf8b85f10e66fe1de
Garvit Yadav <48787184+garvityadav@users.noreply.github.com>
2022-03-07 11:49:19 +0530

    Create Request-header-parser.js

---
+++ Request-header-parser.js
@@ -0,0 +1,8 @@
+app.get("/api/whoami",(req,res)=>{
+  res.json({ipaddress: req.ip,
+            "language":req.headers["accept-language"],
+            software: req.headers["user-agent"],
+               
+    })
+
+});
